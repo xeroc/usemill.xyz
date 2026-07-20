36d188e82495a5f4bb74651c8090a7e7d69d38e3
xeroc <xeroc@users.noreply.github.com>
2026-07-20 14:32:56 +0000

    deploy: xeroc/mill@32d1516c2f35a09c2f4b486f7af172650d111f9a

--- assets/src-UW24ZMRV-Bs9xubU1.js
+++
@@ -1,1 +0,0 @@
-import{f as o}from"./ui-vendor-CrQbBrPR.js";import"./wallet-adapter-B_acGqUc.js";import"./solana-vendor-BAllVuDR.js";var m=o;export{m as default};
---
+++ assets/src-UW24ZMRV-DgHAZ9Uw.js
@@ -0,0 +1,1 @@
+import{g as o}from"./ui-vendor-Ig3sL1Yz.js";import"./wallet-adapter-CV3rMHiv.js";import"./solana-vendor-Cjt-LjmI.js";var m=o;export{m as default};
